4f829d5ad8230b9a84d365247174531a11856b68
MuseMatrixFellowship <131457779+MuseMatrixFellowship@users.noreply.github.com>
2025-03-06 11:51:06 -0600

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config) => {
+        config.externals.push('encoding', /* add any other modules that might be causing the error */);
+        return config;
+      },
+};
+
+export default nextConfig;
